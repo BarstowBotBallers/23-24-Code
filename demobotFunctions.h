617d817a707c0f3537ebca64bc6d312945c65bd3
JonDW99 <116831469+JonDW99@users.noreply.github.com>
2023-05-18 18:10:31 -0700

    Update demobotFunctions.h

--- demobotFunctions.h
+++ demobotFunctions.h
@@ -1,1 +1,12 @@
+#include <kipr/wombat.h>
+// Left motor port - 3
+// Right motor port - 0
+int clawArm = 0;
+int clawHand = 2;
 
+void driveForward(float x, float y){
+    y = y * 1000;
+    motor(0, x);
+    motor(3, x);
+    msleep(y);
+}
