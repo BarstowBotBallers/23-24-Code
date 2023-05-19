b883b894023e6fc5365535455e464e990d876fd7
JonDW99 <116831469+JonDW99@users.noreply.github.com>
2023-05-18 18:40:40 -0700

    Update demobotMeasure.c

--- demobotMeasure.c
+++ demobotMeasure.c
@@ -1,20 +1,17 @@
 #include <kipr/wombat.h>
 #include <stdio.h>
 
-#define clawArm 0
-#define clawHand 2
-#define leftM 0
-#define rightM 3
-
-int measure(){
-    while (digital(0) ==0){
-        motor(leftM, 25);
-        motor(rightM, 25);
+void measure(){
+    clear_motor_position_counter(0);
+    while (digital(0) == 0){
+        motor(0, 25);
+        motor(3, 25);
     }
+    printf(get_motor_positon_counter(0));
 }
 
 int main()
 {
-    
+    measure();
     return 0;
 }
